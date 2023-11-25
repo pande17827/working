a2c60505cdf9a34e2165ae7159db58248fbfed02
VIKAS KUMAR <45943546+pande17827@users.noreply.github.com>
2023-11-25 13:20:39 +0530

    app.py

--- app.py
+++ app.py
@@ -16,12 +16,20 @@ def download_and_load_pkl(file_id, file_name):
         temp_file_path = temp_file.name
     
     # Check if the file already exists locally
-    if not os.path.exists(temp_file_path):
+    if not os.path.exists(temp_file_path) or os.path.getsize(temp_file_path) == 0:
         st.info(f"Downloading {file_name}...")
         with st.spinner('Downloading...'):
             gdown.download(file_url, output=temp_file_path, quiet=False)
-        return {"status": "Downloaded", "file_path": temp_file_path, "file_exists": True}
+            
+        # Check if the download was successful
+        if os.path.getsize(temp_file_path) == 0:
+            st.error(f"Error: The file {file_name} was not downloaded correctly.")
+            return {"status": "Error", "file_path": temp_file_path, "file_exists": False}
+        else:
+            st.success(f"Downloaded {file_name} successfully.")
+            return {"status": "Downloaded", "file_path": temp_file_path, "file_exists": True}
     else:
+        st.info(f"{file_name} already exists. Using the local file.")
         return {"status": "Using local file", "file_path": temp_file_path, "file_exists": True}
 
 # Usage of the function outside
@@ -43,6 +51,8 @@ if os.path.exists(movies_result['file_path']) and os.path.getsize(movies_result[
         movies_dict = pickle.load(f)
 else:
     st.error(f"Error: The file {movies_result['file_path']} is either empty or doesn't exist.")
+    # Provide a placeholder dataset
+    movies_dict = {"title": [], "genre": [], "release_date": []}
 
 # Continue with the rest of your Streamlit app code...
 # Download and load movies_dic
@@ -54,5 +64,7 @@ if os.path.exists(similarity_result['file_path']) and os.path.getsize(similarity
         similarity = pickle.load(f)
 else:
     st.error(f"Error: The file {similarity_result['file_path']} is either empty or doesn't exist.")
+    # Provide a placeholder similarity matrix
+    similarity = pd.DataFrame()
 
 # Continue with the rest of your Streamlit app code...
