b2ad664f6eada844277494ec13cb0eea92df09f6
VIKAS KUMAR <45943546+pande17827@users.noreply.github.com>
2023-11-25 13:17:57 +0530

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import requests
 import os
 import tempfile
 
-@st.cache(allow_output_mutation=True)
 def download_and_load_pkl(file_id, file_name):
     file_url = f"https://drive.google.com/uc?id={file_id}"
     
@@ -21,24 +20,29 @@ def download_and_load_pkl(file_id, file_name):
         st.info(f"Downloading {file_name}...")
         with st.spinner('Downloading...'):
             gdown.download(file_url, output=temp_file_path, quiet=False)
+        return {"status": "Downloaded", "file_path": temp_file_path, "file_exists": True}
     else:
-        st.info(f"{file_name} already exists. Using the local file.")
+        return {"status": "Using local file", "file_path": temp_file_path, "file_exists": True}
 
-    # Debugging: Print file path and existence
-    st.write(f"Temp File Path ({file_name}): {temp_file_path}")
-    st.write(f"File Exists: {os.path.exists(temp_file_path)}")
+# Usage of the function outside
+movies_result = download_and_load_pkl("1lTLT6oVHcbBsacA43pc4VAsS932TjW0J", 'movies_dic.pkl')
+similarity_result = download_and_load_pkl("1sdRQptMMhYVyCJEDM3afS1V9B-qsSCm4", 'similarity_pkl.pkl')
 
-    with open(temp_file_path, 'rb') as f:
-        return pickle.load(f)
+# Display information using Streamlit functions
+st.info(movies_result["status"])
+st.write(f"Movies File Path: {movies_result['file_path']}")
+st.write(f"Movies File Exists: {movies_result['file_exists']}")
 
-# Rest of your Streamlit app code...
+st.info(similarity_result["status"])
+st.write(f"Similarity File Path: {similarity_result['file_path']}")
+st.write(f"Similarity File Exists: {similarity_result['file_exists']}")
 
-# Use download_and_load_pkl in your app
+# Continue with the rest of your Streamlit app code...
 # Download and load movies_dic
-movies_dict = download_and_load_pkl("1lTLT6oVHcbBsacA43pc4VAsS932TjW0J", 'movies_dic.pkl')
+movies_dict = pickle.load(open(movies_result['file_path'], 'rb'))
 movies = pd.DataFrame(movies_dict)
 
 # Download and load similarity_pkl
-similarity = download_and_load_pkl("1sdRQptMMhYVyCJEDM3afS1V9B-qsSCm4", 'similarity_pkl.pkl')
+similarity = pickle.load(open(similarity_result['file_path'], 'rb'))
 
 # Continue with the rest of your Streamlit app code...
